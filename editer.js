6cd99330e43c9da9ee05ec2e768b2878bf01c903
BrandtNorwood <brandtnorwood@gmail.com>
2024-04-19 16:56:18 -0500

    Gutted Editer for Rework

--- editer.js
+++ editer.js
@@ -11,159 +11,20 @@ var timeOptions = { hour12: false };
 //Loads Preveiw pane. Most of this code is stolen from the table generator
 function loadPreview(passedEmpData){
     //disabled for rework
-    return;
-
-    var employeeSelected = document.getElementById("empSelect").selectedIndex
-    if(employeeSelected < 0) {employeeSelected = 0;}
-    
-    if(!passedEmpData){
-        selectedEmployee = fileData[employeeSelected];
-    }
-
-    var unfilteredGenData = parseWeek(fileData);
-    var filteredGenData = filterPTO(unfilteredGenData);
-
-    //Create table HTML object
-    var table = document.createElement('table');
-    table.setAttribute("id","table");
-
-    //Use these to define feilds once instead of 500 times
-    const feilds = ['Name','Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
-
-    //Creates Top lables
-    let topLabels = document.createElement('tr'); 
-    topLabels.style.background = "#c5c5c5"
-    for (let i = 0; i < feilds.length; i++){
-        let labelElement = document.createElement('th');
-        if (feilds[i] == "Name"){
-            labelElement.appendChild(document.createTextNode(feilds[i]));
-        } else{
-            let thisDate = generateDay(i-1,new Time('0000'))
-            labelElement.appendChild(document.createTextNode(
-                feilds[i] + " " + (thisDate.getMonth()+1) + "/" + thisDate.getDate() + "/" + thisDate.getFullYear()
-            ));
-        }
-        topLabels.appendChild(labelElement);
-    }
-    table.appendChild(topLabels);
-
-    let employee = filteredGenData[employeeSelected];
-
-    let empElement = document.createElement('tr');
-
-    feilds.forEach(feild =>{
-        feildElement = document.createElement('th');
-
-        if (employee[feild] && !employee[feild].inactive) {
-            if(employee[feild].filtered){}
-            else {feildElement.style.background = pickColor(employee.Index);}
-
-            if(employee[feild].changed){feildElement.style.textDecoration = "underline"; }
-
-            if (feild == "Name"){
-                feildElement.appendChild(document.createTextNode(employee[feild]));
-            }else{
-                feildElement.appendChild(document.createTextNode(outputDay(employee[feild])));
-            }
-        }
-        empElement.appendChild(feildElement);
-    });
-    table.appendChild(empElement);
-
-    //Reloads Page Elements
-    clearPTOEdit();
-    loadTimeEditor();
-    populatePTOSelect();
-
-    //clear outputPane, attach table
-    document.getElementById("previewPane").replaceChildren();
-    document.getElementById("previewPane").appendChild(table);
 }
 
 
 
 //to load times and checkboxes
 function loadTimeEditor(){  
-
     //disabled for rework
-    return;
-
-    const days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
-
-    //Populates name feild
-    document.getElementById("nameFeild").value = selectedEmployee.Name;
-
-    //populates time editors
-    days.forEach(day => {
-        //if this day is currently scheduled
-        if (selectedEmployee[day]){
-            //Check the box for the day and fill in the time feilds
-            
-            if (!selectedEmployee[day].inactive) {
-                //if the shift should be selected then check box and make sure its not grayed out
-                document.getElementById((day + "On")).checked = true;
-                document.getElementById((day+"Times")).classList.remove("inactive");
-            } else {
-                //if the shift is an old inactive one gray it out and uncheck
-                document.getElementById((day + "On")).checked = false;
-                document.getElementById((day+"Times")).classList.add("inactive");
-            }
-
-            document.getElementById(("start" + day)).value = (
-                selectedEmployee[day].startTime.hour.padStart(2, '0') + ":" + selectedEmployee[day].startTime.minute.padStart(2, '0'));
-            document.getElementById(("end" + day)).value = (
-                selectedEmployee[day].endTime.hour.padStart(2, '0') + ":" + selectedEmployee[day].endTime.minute.padStart(2, '0'));
-        } else {
-            //Uncheck box and set time feilds to their defaults
-            document.getElementById((day + "On")).checked = false;
-            document.getElementById(("start" + day)).value = "";
-            document.getElementById(("end" + day)).value = "";
-
-            //Grays out the time selectors
-            document.getElementById((day+"Times")).classList.add("inactive");
-        }
-    });
 }
 
 
 
 //Takes times from the edit panel and populates the fileData Array
 function saveShiftChanges(){
-
     //disabled for rework
-    return;
-    
-    
-    const days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
-    let nameFeild = document.getElementById("nameFeild").value;
-
-    //Handles renaming employees 
-    if (nameFeild != selectedEmployee.Name){
-        if(confirm(`Change name from ${selectedEmployee.Name} to ${nameFeild}?`)){
-            selectedEmployee.Name = nameFeild;
-        }
-    }
-
-    days.forEach(day =>{
-        if(document.getElementById((day+"On")).checked){
-
-            let startTimeSelected = convertFeildToTime(document.getElementById(("start" + day)).value);
-            let endTimeSelected = convertFeildToTime(document.getElementById(("end" + day)).value);
-
-            selectedEmployee[day] = {startTime:startTimeSelected, endTime:endTimeSelected}
-        } else {
-            if (selectedEmployee[day]){
-                selectedEmployee[day].inactive = true;
-            }
-        }
-    })
-
-    if(onlineMode){
-        saveRemoteEmployee(selectedEmployee);
-    }
-    else{
-        loadEditor();//reload to show result
-    }
 }
 
 
@@ -212,45 +73,6 @@ function populatePTOSelect(){
 //Handles deleting employees.
 function deleteEmployee(){
     //disable function till rework
-    return;
-
-    if (confirm(`Are you sure you want to remove ${selectedEmployee.Name}`)){
-        if (onlineMode){
-            if (onlineMode == "authOn" && userCred.username.length == 0){
-                userCred.username = prompt("Enter your username:");
-                userCred.password = prompt("Enter your password:");
-            }
-        
-            fetch(Url + "/removeEMP",{
-                method:"DELETE",
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify({userCred, selectedEmployee})
-            }) 
-            .then(response => response.json())
-            .then(response => {
-                if(response.authenticated){
-                    getRemoteData().then(function(){
-                        document.getElementById("empSelect").selectedIndex = -1
-                        loadEMPSelect();
-                        loadEditor();
-                    });
-                } else {
-                    alert("Username or Password was incorrect!");
-                    userCred.username = "";
-                    saveRemoteEmployee(selectedEmployee);
-                }
-            });
-
-        }else{
-            //If in offline mode simply remove the employee from the array
-            let index = fileData.indexOf(selectedEmployee);
-            fileData.splice(index, 1);
-
-            document.getElementById("empSelect").selectedIndex = -1
-            loadEMPSelect();
-            loadEditor();
-        }
-    }
 }
 
 
